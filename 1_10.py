44d629753a4bfd356bf96e00b945796bca3175cf
Antonio <dyachkov1984@inbox.ru>
2022-09-25 23:53:41 +0300

    Seminar 1 Case 10

---
+++ 1_10.py
@@ -0,0 +1,15 @@
+#Найти расстояние между двумя 
+# точками пространства.
+
+x_coord = float(input("введите координату первого числа х="))
+y_coord = float(input("введите координату первого числа у="))
+x_coord2 = float(input("введите координату второго числа х="))
+y_coord2 = float(input("введите координату второго числа у="))
+
+def lens(x1, y1, x2, y2):
+    res = round(((x2 - x1) ** 2 + ((y2 - y1)) ** 2) ** 0.5, 2)
+    return res
+
+result = lens(x_coord, y_coord, x_coord2, y_coord2)
+
+print(result)
