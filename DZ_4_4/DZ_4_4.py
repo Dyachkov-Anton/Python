02298ce7608b49fc5255b01b87f2549d78ed97f2
Antonio <dyachkov1984@inbox.ru>
2022-10-10 15:50:19 +0300

    DZ 4 Case 4

---
+++ DZ_4_4/DZ_4_4.py
@@ -0,0 +1,31 @@
+#Задана натуральная степень k. Сформировать случайным образом 
+# список коэффициентов (значения от 0 до 100) многочлена и 
+# записать в файл многочлен степени k.
+# k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+import random
+
+k = int(input('Введите коэффициент: '))
+a = int(random.randint(0,100))
+b = int(random.randint(0,100))
+c = int(random.randint(0,100))
+
+if a != 0:
+    first = (str(a) + "x^" + str(k) + " + ")
+else:
+    first = (str())
+
+if b != 0:
+    second = (str(b) + "x" + " + ")
+else:
+    second = (str())
+
+if c != 0:
+    third = (str(c) + " = 0")
+else:
+    third = (str())
+
+print(f'{first}{second}{third}')
+
+with open('file_res.txt', 'a') as m:
+        m.write(f'{first}{second}{third}\n')
