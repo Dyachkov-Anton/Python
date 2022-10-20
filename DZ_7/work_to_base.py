19f57a5270344fe4902ac9384d406f024bd5b9cc
Antonio <dyachkov1984@inbox.ru>
2022-10-20 16:04:51 +0300

    DZ 7

---
+++ DZ_7/work_to_base.py
@@ -0,0 +1,19 @@
+def read_data():
+     with open('database.txt','r',encoding="utf-8") as file:
+        data = file.readlines()
+        data_phone=[]
+        for line in data:
+            data_phone.append(line)
+        return data_phone
+
+def write_base(data):
+
+    with open('database.txt', 'a',encoding="utf-8") as file:
+     file.write(f'{data}\n')
+        
+def export_to_csv():
+    baza= read_data()
+    with open('export.csv', 'w',encoding="utf-8") as file:
+        for i in baza:
+            file.write(f'{i}')
+    print("Экспорт завершен") 
